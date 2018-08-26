767214afcb0b0dd2f2731941ad976ec1c3ea30d9
Thomas Mader <thomas.mader@gmail.com>
2018-08-26 21:16:16 +0200

    Let's get started

---
+++ src/getting_started/hello_window_clear/main.d
@@ -0,0 +1,77 @@
+module learnopengl.getting_started.hello_window.main;
+
+import derelict.opengl3.gl3;
+import derelict.glfw3.glfw3;
+import std.stdio;
+
+const uint SCR_WIDTH = 800;
+const uint SCR_HEIGHT = 600;
+
+void main()
+{
+    writefln("setup OpenGL context with glfw");
+    DerelictGL3.load();
+    DerelictGLFW3.load();
+    scope(exit) {
+        glfwTerminate();
+    }
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    version(OSX) {
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    }
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", null, null);
+    if (window == null) {
+    writefln("Failed to create GLFW window");
+        return;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
+    writefln("setup OpenGL context with glfw done");
+
+    DerelictGL3.reload();
+    writefln("OpenGL Version %s loaded", DerelictGL3.loadedVersion);
+
+    writefln("init done, using OpenGL now");
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+extern(C) void framebuffer_size_callback(GLFWwindow* window, int width, int height) nothrow
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
